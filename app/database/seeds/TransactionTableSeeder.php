d3e548f1c59d5a87f76f3b4c30da85d95043d1eb
Raymond Chandra <ray_flare@yahoo.com>
2014-10-09 22:37:51 +0700

    status transaction

--- app/database/seeds/TransactionTableSeeder.php
+++ app/database/seeds/TransactionTableSeeder.php
@@ -11,13 +11,19 @@ class TransactionTableSeeder extends Seeder {
 
 		foreach(range(1, 10) as $index)
 		{
+			$status = '';
+			$rand = $faker->numberBetween(1,4);
+			if(rand == 1) $status = 'pending';
+			else if(rand == 2) $status = 'onProcess';
+			else if(rand == 3) $status = 'onShipping';
+			else if(rand == 4) $status = 'completed';
 			Transaction::create([
 				
 			'invoice' => $faker->randomNumber(7),
 			'account_id' => $faker->numberBetween(1,10),
 			'total_price' => $faker->numberBetween(1000,100000),
 			'voucher_id' => $faker->numberBetween(1,10),
-			'status' => $faker->word,
+			'status' => $status,
 			'paid' => $faker->numberBetween(0,1),
 			'shipment_id' => $faker->numberBetween(1,10)
 			
