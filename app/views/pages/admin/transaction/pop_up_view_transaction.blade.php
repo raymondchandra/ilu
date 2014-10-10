16249dca43c54e1e6699c00057f8cf811c06670f
domiworksPC <domiworks@gmail.com>
2014-10-10 21:25:27 +0700

    Sip

--- app/views/pages/admin/transaction/pop_up_view_transaction.blade.php
+++ app/views/pages/admin/transaction/pop_up_view_transaction.blade.php
@@ -3,7 +3,7 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-				<h4 class="modal-title" id="myModalLabel">Tambah Promosi Baru</h4>
+				<h4 class="modal-title" id="myModalLabel">Informasi Transaction</h4>
 			</div>
 			<form class="form-horizontal" role="form">
 				<div class="modal-body">
