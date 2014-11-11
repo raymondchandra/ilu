6c3f9ed5959bafa3f798c338ed3b29f1f2276bf3
Jeffry <stephanus_jeffry@yahoo.com>
2014-11-11 17:31:55 +0700

    manage report produk

--- app/views/pages/admin/report/manage_report_produk.blade.php
+++ app/views/pages/admin/report/manage_report_produk.blade.php
@@ -50,9 +50,7 @@
 					<ul class="nav nav-tabs" role="tablist" style="border: 0px;">
 						<li role="presentation" class="active pull-right"><a href="#3" role="tab" data-toggle="tab" style="">Lakunya Sebuah Produk</a></li>
 						<span class="clearfix "></span>
-						<!--<li role="presentation" class="pull-right"><a href="#4" id='cms_news' role="tab" data-toggle="tab" style="">News</a></li>
-						<span class="clearfix "></span>
-						<li role="presentation" class="pull-right"><a href="#5" id='cms_slideshow' role="tab" data-toggle="tab" style="">Slideshow</a></li>-->
+						<li role="presentation" class="pull-right"><a href="#4" id='cms_news' role="tab" data-toggle="tab" style="">News</a></li>
 					</ul>
 				</div>
 				<!-- Tab panes -->
@@ -92,35 +90,34 @@
 										<tbody class="f_lakunya_sebuah_produk">
 											<tr>
 												<td>
-													<select class="form-control">
-														<option value="">Produk A</option>
-														<option value="">Produk B</option>
-														<option value="">Produk C</option>
-														<option value="">Produk X</option>
+													<select class="form-control idPro">
+														@foreach($hsl2 as $key )
+															<option value="{{$key->id}}">{{$key->name}}</option>
+														@endforeach
 													</select>
 												</td>
 												<td>
-													<select class="form-control">
-														<option value="">Januari</option>
-														<option value="">Februari</option>
-														<option value="">Maret</option>
-														<option value="">April</option>
-														<option value="">Mei</option>
-														<option value="">Juni</option>
-														<option value="">Juli</option>
-														<option value="">Agustus</option>
-														<option value="">September</option>
-														<option value="">Oktober</option>
-														<option value="">November</option>
-														<option value="">Desember</option>
+													<select class="form-control bulanRepPro">
+														<option value="January">Januari</option>
+														<option value="February">Februari</option>
+														<option value="March">Maret</option>
+														<option value="April">April</option>
+														<option value="May">Mei</option>
+														<option value="June">Juni</option>
+														<option value="July">Juli</option>
+														<option value="August">Agustus</option>
+														<option value="September">September</option>
+														<option value="October">Oktober</option>
+														<option value="November">November</option>
+														<option value="December">Desember</option>
 													</select>
 												</td>
 												<td>
-													<select class="form-control">
+													<select class="form-control tahunRepPro">
 														<?php
 														for($i = 0; $i<50; $i++){
 															?>
-															<option value=""><?php echo(2015-$i); ?></option>
+															<option value="<?php echo(date('Y')-$i);?>"><?php echo(date('Y')-$i); ?></option>
 															<?php
 														}
 														?>
@@ -128,7 +125,7 @@
 													</select>
 												</td>
 												<td>
-													<button type="button" class="btn btn-success">Show</button>
+													<button type="button" class="btn btn-success procBut">Show</button>
 												</td>
 											</tr>
 										</tbody>
@@ -141,19 +138,12 @@
 												Date
 											</th>
 											<th>
-												Count Penjualan
+												Penjualan
 											</th>
 										</tr>
 									</thead>
 									<tbody class="f_lakunya_sebuah_produk_hasil">
-										<tr data-toggle="modal" data-target=".pop_up_view_report_produk_detail">
-											<td>
-												45 45 3454
-											</td>
-											<td>
-												Count Penjualan
-											</td>
-										</tr>
+										
 									</tbody>
 								</table>
 							</div>
@@ -163,10 +153,13 @@
 										<thead>
 											<tr>
 												<th>
-													Dari Tanggal
+													Nama Produk
 												</th>
 												<th>
-													Sampai Tanggal
+													Bulan
+												</th>
+												<th>
+													Tahun
 												</th>
 												<th>
 													Command 
@@ -176,32 +169,45 @@
 										<tbody class="f_lakunya_sebuah_produk">
 											<tr>
 												<td>
-													<input type='text' class="form-control"  id='datepicker00_lakunya_sebuah_produk'/>
-													<input type="hidden">
+													<select class="form-control idPro">
+														@foreach($hsl2 as $key )
+															<option value="{{$key->id}}">{{$key->name}}</option>
+														@endforeach
+													</select>
 												</td>
 												<td>
-													<input type='text' class="form-control"  id='datepicker01_lakunya_sebuah_produk'/>
-													<input type="hidden">
+													<select class="form-control bulanRepPro">
+														<option value="January">Januari</option>
+														<option value="February">Februari</option>
+														<option value="March">Maret</option>
+														<option value="April">April</option>
+														<option value="May">Mei</option>
+														<option value="June">Juni</option>
+														<option value="July">Juli</option>
+														<option value="August">Agustus</option>
+														<option value="September">September</option>
+														<option value="October">Oktober</option>
+														<option value="November">November</option>
+														<option value="December">Desember</option>
+													</select>
 												</td>
 												<td>
-													<button type="button" class="btn btn-success">
-														Show
-													</button>
+													<select class="form-control tahunRepPro">
+														<?php
+														for($i = 0; $i<50; $i++){
+															?>
+															<option value="<?php echo(date('Y')-$i);?>"><?php echo(date('Y')-$i); ?></option>
+															<?php
+														}
+														?>
+
+													</select>
+												</td>
+												<td>
+													<button type="button" class="btn btn-success procBut">Show</button>
 												</td>
 											</tr>
 										</tbody>
-										<script type="text/javascript">
-										$(function () {
-											$('#datepicker00_lakunya_sebuah_produk').datepicker({
-												format:"dd-MM-yyyy"
-
-											});
-											$('#datepicker01_lakunya_sebuah_produk').datepicker({
-												format:"dd-MM-yyyy"
-												
-											});
-										});
-										</script>
 									</table>
 								</form>		
 								<table class="table table-bordered table-striped">
@@ -215,7 +221,7 @@
 											</th>
 										</tr>
 									</thead>
-									<tbody class="f_lakunya_sebuah_produk_hasil">
+									<tbody class="f_lakunya_sebuah_produk_hasil2">
 										<tr data-toggle="modal" data-target=".pop_up_view_report_produk_detail">
 											<td>
 												45 45 3454
@@ -243,12 +249,165 @@
 
 							<!-- --------------------------------------------------------------------------------------------------------------------------------------------------------------- -->				
 						</div>
-						<!--<div role="tabpanel" class="tab-pane fade" id="4">
-							{{-- @include('pages.admin.cms.edit_news') --}}							
+						
+						<div role="tabpanel" class="tab-pane fade" id="4">
+							<!-- --------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+							<h3 class="pull-left">
+								Daftar Informasi 
+
+							</h3>
+							<label class="radio-inline pull-right" style="margin-top: 20px;">
+								<input type="radio" name="f_radio_pilih_lakunya_sebuah_produk" id="" value="range"> Range
+							</label>
+							<label class="radio-inline pull-right" style="margin-right: 20px; margin-top: 20px;">
+								<input type="radio" name="f_radio_pilih_lakunya_sebuah_produk" id="" value="one_month" checked> Per 1 Bulan
+							</label>
+							<div id="f_lakunya_sebuah_produk_bulan">
+								<form class="form-horizontal" role="form">
+									<table class="table table-bordered">
+										<thead>
+											<tr>
+												<th>
+													Dari Tanggal
+												</th>
+												<th>
+													Sampai Tanggal
+												</th>
+												<th>
+													Command 
+												</th>
+											</tr>
+										</thead>
+										<tbody class="f_lakunya_sebuah_produk">
+											<tr>
+												<td>
+													<input type='text' class="form-control"  id='datepicker00_lakunya_sebuah_produk'/> 
+												</td>
+												<td>
+													<input type='text' class="form-control"  id='datepicker01_lakunya_sebuah_produk'/>
+												</td>
+												<td>
+													<button type="button" class="btn btn-success butRepPro">
+														Show
+													</button>
+												</td>
+											</tr>
+										</tbody>
+										<script type="text/javascript">
+										$(function () {
+											$('#datepicker00_lakunya_sebuah_produk').datepicker({
+												format:"dd-MM-yyyy"
+											});
+											$('#datepicker01_lakunya_sebuah_produk').datepicker({
+												format:"dd-MM-yyyy"
+												
+											});
+										});
+										</script>
+									</table>
+								</form>		
+								<table class="table table-bordered table-striped">
+									<thead>
+										<tr>
+											<th>
+												Date
+											</th>
+											<th>
+												Penjualan
+											</th>
+										</tr>
+									</thead>
+									<tbody class="f_lakunya_sebuah_produk_hasil">
+										
+									</tbody>
+								</table>
+							</div>
+							<div id="f_lakunya_sebuah_produk_range" class="hidden">
+								<form class="form-horizontal" role="form">
+									<table class="table table-bordered">
+										<thead>
+											<tr>
+												<th>
+													Dari Tanggal
+												</th>
+												<th>
+													Sampai Tanggal
+												</th>
+												<th>
+													Command 
+												</th>
+											</tr>
+										</thead>
+										<tbody class="f_lakunya_sebuah_produk">
+											<tr>
+												<td>
+													<input type='text' class="form-control"  id='datepicker00_lakunya_sebuah_produk'/> 
+												</td>
+												<td>
+													<input type='text' class="form-control"  id='datepicker01_lakunya_sebuah_produk'/>
+												</td>
+												<td>
+													<button type="button" class="btn btn-success butRepPro">
+														Show
+													</button>
+												</td>
+											</tr>
+										</tbody>
+										<script type="text/javascript">
+										$(function () {
+											$('#datepicker00_lakunya_sebuah_produk').datepicker({
+												format:"dd-MM-yyyy"
+											});
+											$('#datepicker01_lakunya_sebuah_produk').datepicker({
+												format:"dd-MM-yyyy"
+												
+											});
+										});
+										</script>
+									</table>
+								</form>		
+								<table class="table table-bordered table-striped">
+									<thead>
+										<tr>
+											<th>
+												Date
+											</th>
+											<th>
+												Count Penjualan
+											</th>
+										</tr>
+									</thead>
+									<tbody class="f_lakunya_sebuah_produk_hasil2">
+										<tr data-toggle="modal" data-target=".pop_up_view_report_produk_detail">
+											<td>
+												45 45 3454
+											</td>
+											<td>
+												Count Penjualan
+											</td>
+										</tr>
+									</tbody>
+								</table>		
+							</div>
+							<script>
+							$('body').on('change', 'input:radio[name="f_radio_pilih_lakunya_sebuah_produk"]', function() {
+
+								if ($(this).is(':checked') && $(this).val() == 'one_month') {
+									$('#f_lakunya_sebuah_produk_range').addClass('hidden');
+									$('#f_lakunya_sebuah_produk_bulan').removeClass('hidden');
+								}else{
+									$('#f_lakunya_sebuah_produk_range').removeClass('hidden');
+									$('#f_lakunya_sebuah_produk_bulan').addClass('hidden');
+								}
+
+							});
+							</script>
+
+							<!-- --------------------------------------------------------------------------------------------------------------------------------------------------------------- -->				
 						</div>
-						<div role="tabpanel" class="tab-pane fade" id="5">
-							{{-- @include('pages.admin.cms.edit_slideshow') --}}					
-						</div>-->
+						
+												
+						</div>
 					</div>
 				</div>
 			</div>
@@ -262,158 +421,104 @@
 @include('pages.admin.report.pop_up_view_report_produk_detail')
 
 <script>
-$('body').on('click','#rangeClick',function(){
-			//$('#datepicker00').html("");
-			//$('#datepicker01').html("");
+	$('body').on('click','.procBut',function(){
+	$bln = $('.bulanRepPro').val();
+	$thn = $('.tahunRepPro').val();
+	$id = $('.idPro').val();
+	$.ajax({
+		type: 'GET',
+		url: '{{URL::route('jeffry.getReportProduk1Month')}}',
+		data: {	
+			"bulanRepPro": $bln,
+			"tahunRepPro" : $thn,
+			"idPro" : $id
+		},
+		success: function(response){
+			if(response['code'] == '404')
+			{
+				alert('failed');
+				location.reload();
+			}
+			else
+			{
+				var tab ="";
+				var obj = response['messages'];
+				//alert(obj);
+				var responses = obj;
+				var tab = "";
+				$(responses).each(function() {
+						tab+= "<input type='hidden'  value='"+(this)['tanggal']+"-"+(this)['bulan']+"'/>";
+						tab+= "<tr data-toggle='modal' data-target='.pop_up_view_report_produk_detail' class='viewDetTgl'>";
+						tab+="<td>"+(this)['tanggal']+"-"+(this)['bulan']+"</td>";
+						tab+="<td>"+toRp((this)['penjualan'])+"</td>";
+						tab+="</tr>";
+					});
+					$('.f_lakunya_sebuah_produk_hasil').html(tab);
+			}
+		},error: function(xhr, textStatus, errorThrown){
+			alert("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
+			alert("responseText: "+xhr.responseText);
+		}
+	},'json');
+
+});
+
+$('body').on('click','.viewDetTgl',function(){
+			$tgl = $(this).prev().val();
+			$id = $('.idPro').val();
+			$('#myModalLabelDet').text("Detail Produk "+ $('.idPro').find(":selected").text()+ " Pada Tanggal "+$tgl);
 			$.ajax({
 				type: 'GET',
-				url: "{{URL::route('jeffry.getReport')}}",
+				url: '{{URL::route('jeffry.getReportProduk1MonthDetail')}}',
 				data: {	
-					"reportBy": "range",
-					"date1" : $('#datepicker00').val(),
-					"date2" : $('#datepicker01').val()
+					"tgl": $tgl,
+					"id" : $id
 				},
 				success: function(response){
 					if(response['code'] == '404')
 					{
-							//$('#wishlistcontent').append("<td>agent tidak ditemukan</td>");
-							
-						}
-						else
-						{
-							var respon = response['hasil2'];
-							var pjln = '';
-							var tgl='';
-							var bln='';
-							var ket='';
-							for(i=0; i < respon.length;i++)
-							{
-								if(i == 0)
-								{
-									pjln = pjln + (respon[i].penjualan);
-									tgl = tgl + (respon[i].tanggal);
-									
-								}else
-								{
-									pjln = pjln + ',' + (respon[i].penjualan);
-									tgl = tgl + ',' + (respon[i].tanggal);
-								}
-								bln = (respon[i].bulan);
-								ket = (respon[i].ket);
-							}
-							$('#penjualanGraf').val(pjln);
-							$('#tanggalGraf').val(tgl);
-							$('#bulanGraf').val(bln);
-							$('#ket').val(ket);
-							
-							var a = $('#penjualanGraf').val();
-							var b = $('#tanggalGraf').val();
-							var c;
-							var d = $('#ket').val();
-							var e;
-							if(d == 'day')
-							{
-								e = 'Report By Day';
-								c = $('#bulanGraf').val().replace('-',' ');
-							}else if(d == 'week')
-							{
-								e = 'Report By Week';
-								c = $('#bulanGraf').val().replace('-',' ');
-							}else if(d == 'month')
-							{
-								e = 'Report By Month';
-								c = $('#bulanGraf').val();
-							}else if(d == 'year')
-							{
-								e = 'Report By Year';
-							}else if(d == 'range')
-							{
-								e = 'Report By Range';
-								c = $('#bulanGraf').val();
-							}
-							b = b.split(",");
-							$('#container').highcharts({
-								chart: {
-									type: 'line'
-								},
-								title: {
-									text: e
-								},
-								subtitle: {
-									text: c
-								},
-								xAxis: {
-									categories: b	
-								},
-								yAxis: {
-									title: {
-										text: 'Rupiah'
-									}
-								},
-								plotOptions: {
-									line: {
-										dataLabels: {
-											enabled: true
-										},
-										enableMouseTracking: false
-									}
-								},
-								series: [{
-									name: 'Penjualan',
-									data: JSON.parse("[" + a + "]")
-								}]
-							});
-							
-							var tab ="";
-							var obj = response['hasil1'];
-							var responses = obj;
-							$(responses).each(function() {
-								tab+= "<tr>";
-								tab+="<td>"+$(this)[0].tanggal2+"</td>";
-								tab+="<td>"+$(this)[0].penjualan+"</td>";
-								tab+="</tr>";
-							});
-							$('#tblrg').html(tab);
-							$('#navigator').html(jQuery.parseJSON(response.links));
-						}
-					},error: function(xhr, textStatus, errorThrown){
-						alert("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
-						alert("responseText: "+xhr.responseText);
+						var tab="";
+						tab+= "<tr>";
+						tab+="<td colspan='2'>"+"No Transactions"+"</td>";
+						tab+="</tr>";
+						
+						$('.detailProdTab').html(tab);
 					}
-				},'json');
-});
-
-$('body').on('click','#navigator ul li a',function(){
-
-	var cek;
-	var link = $(this).attr('href');
-	$.get(link,function(response){
-		try
-		{
-			if(response['hasil2'][0].ket == 'range')
-			{
-				cek = false;
-				var tab ="";
-				var obj = response['hasil1'];
-				var responses = obj;
-				$(responses).each(function() {
-					tab+= "<tr>";
-					tab+="<td>"+$(this)[0].tanggal2+"</td>";
-					tab+="<td>"+$(this)[0].penjualan+"</td>";
-					tab+="</tr>";
-				});
-				$('#tblrg').html(tab);
-				$('#navigator').html(jQuery.parseJSON(response.links));
-			}
-		}catch(e)
-		{
-			window.location.assign(link);
-		}
+					else
+					{
+						var tab = "";
+						var obj = response['messages'];
+						
+						var responses = obj;
+						var idx = 0;
+						$(responses).each(function() {
+								tab+= "<tr>";
+								tab+="<td>"+responses[idx].invoice+"</td>";
+								tab+="<td>"+responses[idx].quantity+"</td>";
+								tab+="<td>"+responses[idx].full_name+"</td>";
+								tab+="</tr>";
+								idx = idx+1;
+							});
+							$('.detailProdTab').html(tab);
+					}
+				},error: function(xhr, textStatus, errorThrown){
+					alert("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
+					alert("responseText: "+xhr.responseText);
+				}
+			},'json');
 	});
 
-	return false;
-
-});
-
+function toRp(angka){
+		var rev     = parseInt(angka, 10).toString().split('').reverse().join('');
+		var rev2    = '';
+		for(var i = 0; i < rev.length; i++){
+			rev2  += rev[i];
+			if((i + 1) % 3 === 0 && i !== (rev.length - 1)){
+				rev2 += '.';
+			}
+		}
+		return 'Rp ' + rev2.split('').reverse().join('')+',-';
+		//return 'IDR ' + rev2.split('').reverse().join('') + ',00';
+	}
 </script>
 @stop
